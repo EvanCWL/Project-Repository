bce281d60f05234243fd708effe8a297c9e78a32
Evan Chua <50959598+EvanCWL@users.noreply.github.com>
2019-08-03 23:26:32 +0800

    Update 0.01
    Starting work progress

--- C++ Training/HelloWorld.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(){
-    std::cout << "Hello World";
-    std::cin.clear(); // reset any error flags
-    return 0;
-}
--- C++ Training/Test 1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(){
-    double i{1.5},x{2.5};
-    std::cout << i << "," << x;
-    return 0;
-}
