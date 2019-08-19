9db6cb950b8784a7aa777a23a72665f487878e05
EvanCWL <50959598+EvanCWL@users.noreply.github.com>
2019-08-19 17:18:28 +0800

    Created vec3

---
+++ Distributed Cloud-based Ray Tracing/RTW/RTW/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include "vec3.h"
+using namespace std;
+
+int main() {
+	ofstream img("NW.ppm");
+	int nx = 200;
+	int ny = 100;
+	img << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2;
+			int ir = int(255 * r);
+			int ig = int(255 * g);
+			int ib = int(255 * b);
+			img << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+}
---
+++ Distributed Cloud-based Ray Tracing/RTW/RTW/vec3.cpp
@@ -0,0 +1,41 @@
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+
+class vec3 {
+public:
+	vec3() {}
+	vec3(double e0, double e1, double e2) {
+		e[0] = e0;
+		e[1] = e1;
+		e[2] = e2;
+	}
+	inline double x() const { return e[0]; }
+	inline double y() const { return e[1]; }
+	inline double z() const { return e[2]; }
+	inline double r() const { return e[0]; }
+	inline double g() const { return e[1]; }
+	inline double b() const { return e[2]; }
+
+	inline const vec3& operator+() const { return *this; }
+	inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+	inline double operator[](int i) const { return e[i]; }
+	inline double& operator[](int i) { return e[i]; };
+
+	inline vec3& operator+=(const vec3 &v2);
+	inline vec3& operator-=(const vec3 &v2);
+	inline vec3& operator*=(const vec3 &v2);
+	inline vec3& operator/=(const vec3 &v2);
+	inline vec3& operator*=(const vec3 &v2);
+	inline vec3& operator/=(const vec3 &v2);
+
+	inline double length() const {
+		return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+	}
+	inline double squared_length() const {
+		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+	}
+	inline void make_unit_vector();
+	double e[3];
+};
+
