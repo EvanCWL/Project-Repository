e52ea25e2ae8aba48b242036539874fcadf54e56
EvanCWL <50959598+EvanCWL@users.noreply.github.com>
2019-08-20 15:19:49 +0800

    Completed second part

--- Distributed Cloud-based Ray Tracing/RTW/RTW/main.cpp
+++ Distributed Cloud-based Ray Tracing/RTW/RTW/main.cpp
@@ -2,13 +2,16 @@
 #include <fstream>
 #include "ray.h"
 
-
 using namespace std;
-
+/*
+unit_vector causes irrational value to occur.
+t goes from 1.0 to 0.0
+*/
 vec3 color(const ray& r) {
-	vec3 unit_direction = unit_vector(r.direction());
+	vec3 unit_direction = r.direction();
 	double t = 0.5*(unit_direction.y() + 1.0);
-	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+	//std::cout << (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0) << "\n";
+	return (1.0-t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -20,7 +23,7 @@ int main() {
 	vec3 horizontal(4.0, 0.0, 0.0);
 	vec3 vertical(0.0, 2.0, 0.0);
 	vec3 origin(0.0, 0.0, 0.0);
-	for (int j = ny - 1; j >= 0; j--) {
+	for (int j = ny; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
 			double u = double(i) / double(nx);
 			double v = double(j) / double(ny);
