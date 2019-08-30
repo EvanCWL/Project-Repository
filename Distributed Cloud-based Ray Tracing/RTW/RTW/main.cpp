6dcd41f004d326723d7a9034bb9aaccfde16a24c
EvanCWL <50959598+EvanCWL@users.noreply.github.com>
2019-08-30 16:29:59 +0800

    Created a controlled RTW.

--- Distributed Cloud-based Ray Tracing/RTW/RTW/main.cpp
+++ Distributed Cloud-based Ray Tracing/RTW/RTW/main.cpp
@@ -1,21 +1,33 @@
-#include <iostream>
 #include <fstream>
 #include "ray.h"
-
 using namespace std;
-/*
-unit_vector causes irrational value to occur.
-t goes from 1.0 to 0.0
-*/
+
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = b * b - 4 * a*c;
+	if (discriminant < 0) {
+		return 0;
+	}
+	else {
+		return (-b - sqrt(discriminant)) / (2.0*a);
+	}
+}
 vec3 color(const ray& r) {
-	vec3 unit_direction = r.direction();
-	double t = 0.5*(unit_direction.y() + 1.0);
-	//std::cout << (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0) << "\n";
-	return (1.0-t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+	float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
+	if (t > 0.0) {
+		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
+		return 0.5*vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+	}
+	vec3 unit_direction = unit_vector(r.direction());
+	t = 0.5*(unit_direction.y() + 1.0);
+	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 int main() {
-	ofstream img("second.ppm");
+	ofstream img("fourth.ppm");
 	int nx = 200;
 	int ny = 100;
 	img << "P3\n" << nx << " " << ny << "\n255\n";
@@ -23,10 +35,10 @@ int main() {
 	vec3 horizontal(4.0, 0.0, 0.0);
 	vec3 vertical(0.0, 2.0, 0.0);
 	vec3 origin(0.0, 0.0, 0.0);
-	for (int j = ny; j >= 0; j--) {
+	for (int j = ny-1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
-			double u = double(i) / double(nx);
-			double v = double(j) / double(ny);
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
 			vec3 col = color(r);
 			int ir = int(255.99 * col[0]);
--- Distributed Cloud-based Ray Tracing/RTW/RTW/vec3.cpp
+++ Distributed Cloud-based Ray Tracing/RTW/RTW/vec3.cpp
@@ -1,41 +1,41 @@
-#include <cmath>
-#include <cstdlib>
+#include <math.h>
+#include <stdlib.h>
 #include <iostream>
 class vec3
 {
 public:
 	vec3() {}
-	vec3(double e0, double e1, double e2) {
+	vec3(float e0, float e1, float e2) {
 		e[0] = e0;
 		e[1] = e1;
 		e[2] = e2;
 	}
-	inline double x() const { return e[0]; }
-	inline double y() const { return e[1]; }
-	inline double z() const { return e[2]; }
-	inline double r() const { return e[0]; }
-	inline double g() const { return e[1]; }
-	inline double b() const { return e[2]; }
+	inline float x() const { return e[0]; }
+	inline float y() const { return e[1]; }
+	inline float z() const { return e[2]; }
+	inline float r() const { return e[0]; }
+	inline float g() const { return e[1]; }
+	inline float b() const { return e[2]; }
 
 	inline const vec3& operator+() const { return *this; }
 	inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-	inline double operator[](int i) const { return e[i]; }
-	inline double& operator[](int i) { return e[i]; };
+	inline float operator[](int i) const { return e[i]; }
+	inline float& operator[](int i) { return e[i]; };
 
 	inline vec3& operator+=(const vec3 &v2);
 	inline vec3& operator-=(const vec3 &v2);
 	inline vec3& operator*=(const vec3 &v2);
 	inline vec3& operator/=(const vec3 &v2);
-	inline vec3& operator*=(const double t);
-	inline vec3& operator/=(const double t);
+	inline vec3& operator*=(const float t);
+	inline vec3& operator/=(const float t);
 
-	inline double length() const {
+	inline float length() const {
 		return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
 	}
-	inline double squared_length() const {
+	inline float squared_length() const {
 		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
 	}
 	inline void make_unit_vector();
-	double e[3];
+	float e[3];
 };
 
