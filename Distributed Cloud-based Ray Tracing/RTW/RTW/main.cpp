12df7e856b0e5ade5269b17c38235d436ec28e1e
EvanCWL <50959598+EvanCWL@users.noreply.github.com>
2019-09-11 12:46:39 +0800

    Completed chapter 7&8

--- Distributed Cloud-based Ray Tracing/RTW/RTW/main.cpp
+++ Distributed Cloud-based Ray Tracing/RTW/RTW/main.cpp
@@ -5,49 +5,52 @@
 #include "hitable_list.h"
 #include "camera.h"
 #include "float.h"
+#include "material.h"
 
 using namespace std;
-vec3 random_in_unit_sphere() {
-	vec3 p;
-	do {
-		p = 2.0*vec3((rand() % 2), (rand() % 2), (rand() % 2)) - vec3(1, 1, 1);
-	} while (p.squared_length() >= 1.0);
-	return p;
-}
 
-vec3 color(const ray& r, hitable *world) {
+
+vec3 color(const ray& r, hitable *world, int depth) {
 	hit_record rec;
 	if (world->hit(r, 0.001, FLT_MAX, rec)) {
-		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-		return 0.5*color(ray(rec.p, target - rec.p), world);
+		ray scattered;
+		vec3 attenuation;
+		if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+			return attenuation * color(scattered, world, depth + 1);
+		}
+		else {
+			return vec3(0, 0, 0);
+		}
 	}
 	else {
 		vec3 unit_direction = unit_vector(r.direction());
 		float t = 0.5*(unit_direction.y() + 1.0);
-		return (1.0-t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+		return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 	}
 }
 
 int main() {
-	ofstream img("seventh.ppm");
+	ofstream img("eighth.ppm");
 	int nx = 200;
 	int ny = 100;
 	int ns = 100;
 	img << "P3\n" << nx << " " << ny << "\n255\n";
-	hitable *list[2];
-	list[0] = new sphere(vec3(0, 0, -1), 0.5);
-	list[1] = new sphere(vec3(0, -100.5, -1), 100);
-	hitable *world = new hitable_list(list, 2);
+	hitable *list[4];
+	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
+	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
+	hitable *world = new hitable_list(list, 4);
 	camera cam;
-	for (int j = ny-1; j >= 0; j--) {
+	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
 			vec3 col(0, 0, 0);
 			for (int s = 0; s < ns; s++) {
-				float u = float(i + (rand() % 2) ) / float(nx);
-				float v = float(j + (rand() % 2) ) / float(ny);
+				float u = float(i + ((float)rand()) / ((float)RAND_MAX + 1)) / float(nx);
+				float v = float(j + ((float)rand()) / ((float)RAND_MAX + 1)) / float(ny);
 				ray r = cam.get_ray(u, v);
 				vec3 p = r.point_at_parameter(2.0);
-				col += color(r,world);
+				col += color(r, world, 0);
 			}
 			col /= float(ns);
 			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
@@ -58,55 +61,3 @@ int main() {
 		}
 	}
 }
-/*
-#include "ray.h"
-#include <fstream>
-using namespace std;
-
-float hit_sphere(const vec3& center, float radius, const ray& r) {
-	vec3 oc = r.origin() - center;
-	float a = dot(r.direction(), r.direction());
-	float b = 2.0 * dot(oc, r.direction());
-	float c = dot(oc, oc) - radius * radius;
-	float discriminant = (b * b - 4 * a*c);
-	if (discriminant < 0) {
-		return -1.0;
-	}
-	else {
-		return (-b - sqrt(discriminant)) / (2.0*a);
-	}
-}
-vec3 color(const ray& r) {
-	float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
-	if (t > 0.0) {
-		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
-		return 0.5*vec3(N.x() + 1, N.y() + 1, N.z() + 1);
-	}
-	vec3 unit_direction = unit_vector(r.direction());
-	t = 0.5*(unit_direction.y() + 1.0);
-	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
-}
-
-int main() {
-	ofstream img("fourth.ppm");
-	int nx = 200;
-	int ny = 100;
-	img << "P3\n" << nx << " " << ny << "\n255\n";
-	vec3 lower_left_corner(-2.0, -1.0, -1.0);
-	vec3 horizontal(4.0, 0.0, 0.0);
-	vec3 vertical(0.0, 2.0, 0.0);
-	vec3 origin(0.0, 0.0, 0.0);
-	for (int j = ny; j >= 0; j--) {
-		for (int i = 0; i < nx; i++) {
-			float u = float(i) / float(nx);
-			float v = float(j) / float(ny);
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			vec3 col = color(r);
-			int ir = int(255.99 * col[0]);
-			int ig = int(255.99 * col[1]);
-			int ib = int(255.99 * col[2]);
-			img << ir << " " << ig << " " << ib << "\n";
-		}
-	}
-}
-*/
--- Distributed Cloud-based Ray Tracing/RTW/RTW/vec3.cpp
+++ Distributed Cloud-based Ray Tracing/RTW/RTW/vec3.cpp
@@ -1,6 +1,5 @@
 #include <math.h>
 #include <stdlib.h>
-
 class vec3
 {
 public:
@@ -33,7 +32,7 @@ public:
 		return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
 	}
 	inline float squared_length() const {
-		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+		return (e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
 	}
 	inline void make_unit_vector();
 	float e[3];
