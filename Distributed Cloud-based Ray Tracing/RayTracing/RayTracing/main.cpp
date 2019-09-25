fd7cdb5648418be3407f1df0081628859a69252f
Evan Chua <50959598+EvanCWL@users.noreply.github.com>
2019-09-26 00:42:31 +0800

    completed material

--- Distributed Cloud-based Ray Tracing/RayTracing/RayTracing/main.cpp
+++ Distributed Cloud-based Ray Tracing/RayTracing/RayTracing/main.cpp
@@ -9,6 +9,33 @@
 
 using namespace std;
 
+hitable* random_scene() {
+	int n = 500;
+	hitable** list = new hitable * [n + 1];
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+	int i = 1;
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			float choose_mat = ((float)rand()) / ((float)RAND_MAX + 1);
+			vec3 center(a + 0.9 * ((float)rand()) / ((float)RAND_MAX + 1), 0.2, b + 0.9 * ((float)rand()) / ((float)RAND_MAX + 1));
+			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+				if (choose_mat < 0.8) {
+					list[i++] = new sphere(center, 0.2, new lambertian(vec3(((float)rand()) / ((float)RAND_MAX + 1) * ((float)rand()) / ((float)RAND_MAX + 1), ((float)rand()) / ((float)RAND_MAX + 1) * ((float)rand()) / ((float)RAND_MAX + 1), ((float)rand()) / ((float)RAND_MAX + 1) * ((float)rand()) / ((float)RAND_MAX + 1))));
+				}
+				else if (choose_mat < 0.95) {
+					list[i++] = new sphere(center,0.2,new metal(vec3(0.5*(1+ ((float)rand()) / ((float)RAND_MAX + 1)), 0.5 * (1 + ((float)rand()) / ((float)RAND_MAX + 1)), 0.5 * (1 + ((float)rand()) / ((float)RAND_MAX + 1))),0.5* ((float)rand()) / ((float)RAND_MAX + 1)));
+				}
+				else {
+					list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+				}
+			}
+		}
+	}
+	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+	list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+	return new hitable_list(list, i);
+}
 
 vec3 color(const ray& r, hitable *world, int depth) {
 	hit_record rec;
@@ -30,20 +57,27 @@ vec3 color(const ray& r, hitable *world, int depth) {
 }
 
 int main() {
-	ofstream img("tenth.ppm");
-	int nx = 200;
-	int ny = 100;
+	ofstream img("twelveth.ppm");
+	int nx = 1920;
+	int ny = 1080;
 	int ns = 100;
 	img << "P3\n" << nx << " " << ny << "\n255\n";
 	hitable *list[5];
 	float R = cos(M_PI / 4);
 	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
 	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
+	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.0));
 	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
 	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
 	hitable *world = new hitable_list(list, 5);
-	camera cam(vec3(-2,2,1),vec3(0,0,-1),vec3(0,1,0),90,float(nx)/float(ny));
+	world = random_scene();
+
+	vec3 lookfrom(13, 3, 2);
+	vec3 lookat(0, 0, 0);
+	//float dist_to_focus = (lookfrom - lookat).length();
+	float dist_to_focus = 10.0;
+	float aperture = 0.1;
+	camera cam(lookfrom,lookat,vec3(0,1,0),20,float(nx)/float(ny),aperture,dist_to_focus);
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
 			vec3 col(0, 0, 0);
