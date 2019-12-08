beceb39cb77e07257787f27d8e56cf7a16c33f31
EvanCWL <50959598+EvanCWL@users.noreply.github.com>
2019-12-08 20:34:07 +0000

    Update

--- Current/SCC.300_RT/SCC.300_RT/main.cu
+++ Current/SCC.300_RT/SCC.300_RT/main.cu
@@ -165,8 +165,8 @@ void saveImage(int width, int height, vec3* fb, string output) {
 }
 
 int main(int argc, char* argv[]) {
-	int width = 1920;
-	int height = 1080;
+	int width = 1280;
+	int height = 720;
 	int ns = 10;
 	int tx = 22;
 	int ty = 22;
