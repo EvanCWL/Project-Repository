be9d8fe7d520f6b02be160e3c97e106f6f96a42a
EvanCWL <50959598+EvanCWL@users.noreply.github.com>
2019-12-08 23:18:33 +0000

    Set

--- Current/SCC.300_RT/SCC.300_RT/main.cu
+++ Current/SCC.300_RT/SCC.300_RT/main.cu
@@ -39,7 +39,7 @@ __device__ vec3 color(const ray& r, hitable** world, curandState* local_rand_sta
 	vec3 cur_attenuation = vec3(1.0, 1.0, 1.0);
 	for (int i = 0; i < 50; i++) {
 		hit_record rec;
-		if ((*world)->hit(cur_ray, 0.1f, FLT_MAX, rec)) {
+		if ((*world)->hit(cur_ray, 0.001f, FLT_MAX, rec)) {
 			ray scattered;
 			vec3 attenuation;
 			if (rec.mat_ptr->scatter(cur_ray, rec, attenuation, scattered, local_rand_state)) {
@@ -90,15 +90,15 @@ __global__ void render(vec3* fb, int max_x, int max_y, int ns, camera** cam, hit
 	}
 	rand_state[pixel_index] = local_rand_state;
 	col /= float(ns);
-	col[0] = sqrt(col[0]);
-	col[1] = sqrt(col[1]);
-	col[2] = sqrt(col[2]);
+	col[0] = 255.99 * sqrt(col[0]);
+	col[1] = 255.99 * sqrt(col[1]);
+	col[2] = 255.99 * sqrt(col[2]);
 	fb[pixel_index] = col;
 }
 
 #define RND (curand_uniform(&local_rand_state))
 
-__global__ void create_world(hitable** d_list, hitable** d_world, camera** d_camera, int width, int height, curandState* rand_state) {
+__global__ void create_world(hitable** d_list, hitable** d_world, camera** d_camera, int width, int height, curandState* rand_state, int xc) {
 	if (threadIdx.x == 0 && blockIdx.x == 0) {
 		curandState local_rand_state = *rand_state;
 		d_list[0] = new sphere(vec3(0, -1000.0, -1), 1000,
@@ -107,7 +107,7 @@ __global__ void create_world(hitable** d_list, hitable** d_world, camera** d_cam
 		for (int a = -11; a < 11; a++) {
 			for (int b = -11; b < 11; b++) {
 				float choose_mat = RND;
-				vec3 center(a + RND, 0.2, b + RND);
+				vec3 center(a + 0.9 * RND, 0.2, b + 0.9 * RND);
 				if (choose_mat < 0.8f) {
 					d_list[i++] = new sphere(center, 0.2,
 						new lambertian(vec3(RND * RND, RND * RND, RND * RND)));
@@ -127,41 +127,21 @@ __global__ void create_world(hitable** d_list, hitable** d_world, camera** d_cam
 		*rand_state = local_rand_state;
 		*d_world = new hitable_list(d_list, 22 * 22 + 1 + 3);
 
-		vec3 lookfrom(13, 2, 3);
+		vec3 lookfrom(xc, 3, 2);
 		vec3 lookat(0, 0, 0);
-		float dist_to_focus = 10.0; (lookfrom - lookat).length();
+		float dist_to_focus = 10.0; //(lookfrom - lookat).length();
 		float aperture = 0.1;
-		*d_camera = new camera(lookfrom,
-			lookat,
-			vec3(0, 1, 0),
-			30.0,
-			float(width) / float(height),
-			aperture,
-			dist_to_focus);
+		*d_camera = new camera(lookfrom, lookat, vec3(0, 1, 0), 20, float(width) / float(height), aperture, dist_to_focus);
 	}
 }
 
 __global__ void free_world(hitable** d_list, hitable** d_world, camera** d_camera) {
-	for (int i = 0; i < 22 * 22 + 1 + 3; i++) {
+	for (int i = 0; i < 500; i++) {
 		delete ((sphere*)d_list[i])->mat_ptr;
 		delete d_list[i];
 	}
 	delete* d_world;
 	delete* d_camera;
-}
-
-void saveImage(int width, int height, vec3* fb, string output) {
-	ofstream img(output);
-	img << "P3\n" << width << " " << height << "\n255\n";
-	for (int j = height - 1; j >= 0; j--) {
-		for (int i = 0; i < width; i++) {
-			size_t pixel_index = j * width + i;
-			int ir = int(255.99 * fb[pixel_index].r());
-			int ig = int(255.99 * fb[pixel_index].g());
-			int ib = int(255.99 * fb[pixel_index].b());
-			img << ir << " " << ig << " " << ib << "\n";
-		}
-	}
 }
 
 int main(int argc, char* argv[]) {
@@ -199,18 +179,12 @@ int main(int argc, char* argv[]) {
 
 	// make our world of hitables & the camera
 	hitable** d_list;
-	int num_hitables = 22 * 22 + 1 + 3;
+	int num_hitables = 500;
 	checkCudaErrors(cudaMalloc((void**)&d_list, num_hitables * sizeof(hitable*)));
 	hitable** d_world;
 	checkCudaErrors(cudaMalloc((void**)&d_world, sizeof(hitable*)));
 	camera** d_camera;
 	checkCudaErrors(cudaMalloc((void**)&d_camera, sizeof(camera*)));
-	create_world << <1, 1 >> > (d_list, d_world, d_camera, width, height, d_rand_state2);
-	checkCudaErrors(cudaGetLastError());
-	checkCudaErrors(cudaDeviceSynchronize());
-
-	// Save image
-	//saveImage(width, height, fb, "output.ppm");
 
 	// Output FB as Image
 
@@ -241,47 +215,12 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	/*
-	SDL_GLContext context = SDL_GL_CreateContext(window);
-	if (context == NULL) {
-		std::cout << SDL_GetError();
-		return 1;
-	}
-
-
-	for (int j = height - 1; j >= 0; j--) {
-		for (int i = 0; i < width; i++) {
-			size_t pixel_index = j * width + i;
-			int ir = int(255.99 * fb[pixel_index].r());
-			int ig = int(255.99 * fb[pixel_index].g());
-			int ib = int(255.99 * fb[pixel_index].b());
-			SDL_SetRenderDrawColor(renderer, ir, ig, ib, 255);
-			SDL_Rect rectangle;
-			rectangle.x = i;
-			rectangle.y = j;
-			rectangle.w = 50;
-			rectangle.h = 50;
-			SDL_RenderFillRect(renderer, &rectangle);
-			SDL_RenderPresent(renderer);
-		}
-	}
-	*/
-
+	
 	//Render Scene
-	clock_t start, stop;
-	start = clock();
 	// Render our buffer
+	int xc = 13;
 	dim3 blocks(width / tx + 1, height / ty + 1);
 	dim3 threads(tx, ty);
-	render_init << <blocks, threads >> > (width, height, d_rand_state);
-	checkCudaErrors(cudaGetLastError());
-	checkCudaErrors(cudaDeviceSynchronize());
-	render << <blocks, threads >> > (fb, width, height, ns, d_camera, d_world, d_rand_state);
-	checkCudaErrors(cudaGetLastError());
-	checkCudaErrors(cudaDeviceSynchronize());
-	stop = clock();
-	double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
-	std::cerr << "took " << timer_seconds << " seconds.\n";
 
 	int j = 0;
 	int i = 0;
@@ -293,23 +232,40 @@ int main(int argc, char* argv[]) {
 			case SDL_QUIT: {
 				running = false;
 			}
-						 break;
+			break;
 			case SDL_WINDOWEVENT: {
 				if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
 					width = (uint32_t)event.window.data1;
 					height = (uint32_t)event.window.data2;
 				}
 			}
-								break;
-								// ... Handle other events here. **IMPLEMENTATION OF MOUSE DETECTIONS**
+			break;
+			case SDL_KEYDOWN: 					// ... Handle other events here. **IMPLEMENTATION OF MOUSE DETECTIONS**
+				switch (event.key.keysym.sym) {
+					case SDLK_LEFT: xc--; std::cout << xc << "\n"; break;
+					case SDLK_RIGHT: xc++; std::cout << xc << "\n"; break;
+					case SDLK_RETURN:
+						clock_t start, stop;
+						start = clock();
+						create_world << <1, 1 >> > (d_list, d_world, d_camera, width, height, d_rand_state2, 13);
+						checkCudaErrors(cudaGetLastError());
+						checkCudaErrors(cudaDeviceSynchronize());
+						render_init << <blocks, threads >> > (width, height, d_rand_state);
+						checkCudaErrors(cudaGetLastError());
+						checkCudaErrors(cudaDeviceSynchronize());
+						render << <blocks, threads >> > (fb, width, height, ns, d_camera, d_world, d_rand_state);
+						checkCudaErrors(cudaGetLastError());
+						checkCudaErrors(cudaDeviceSynchronize());
+						stop = clock();
+						double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+						std::cerr << "took " << timer_seconds << " seconds.\n";
+						break;
+				}
 			}
 		}
 
 		size_t pixel_index = j * width + i;
-		int ir = int(255.99 * fb[pixel_index].r());
-		int ig = int(255.99 * fb[pixel_index].g());
-		int ib = int(255.99 * fb[pixel_index].b());
-		SDL_SetRenderDrawColor(renderer, ir, ig, ib, 255);
+		SDL_SetRenderDrawColor(renderer, fb[pixel_index].r(), fb[pixel_index].g(), fb[pixel_index].b(), 255);
 		SDL_Rect rectangle;
 		rectangle.x = i;
 		rectangle.y = height - (j + 1);
